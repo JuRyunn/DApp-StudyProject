b6bd13423ac82e03aa11f71c13e5919575db8be2
JuRyunn <wngus2991@gmail.com>
2022-08-15 23:37:49 +0900

    SecondCommit
    Transfer Event

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -3,6 +3,33 @@ pragma solidity ^0.5.0;
 contract Tether {
     string public name= 'Tether';
     string public symbol= 'USDT';
-    uint256 public totalSupplay= 1000000000000000000;
+    uint256 public totalSupply= 1000000000000000000;
     uint8 public decimals= 18;
+
+    event Transfer (
+        address _from,
+        address _to,
+        uint _value
+    );
+
+    event Approve (
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() public {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        // add the balance
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
