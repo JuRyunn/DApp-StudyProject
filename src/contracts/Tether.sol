d4eb4dddc1fb825da04522de330088d992127b96
JuRyunn <wngus2991@gmail.com>
2022-08-16 17:12:07 +0900

    FifthCommit
    Truffle Migrate, SmartContract Transaction

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -1,55 +1,55 @@
 pragma solidity ^0.5.0;
 
 contract Tether {
-    string public name= 'JuRyunn Tether Token';
-    string public symbol= 'mUSDT';
-    uint256 public totalSupply= 1000000000000000000;
-    uint8 public decimals= 18;
+    string  public name = "JuRyunn Tether Token";
+    string  public symbol = "mUSDT";
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8   public decimals = 18;
 
-    event Transfer (
-        address _from, 
-        address _to,
+    event Transfer(
+        address indexed _from,
+        address indexed _to, 
         uint _value
     );
 
-    event Approval (
+    event Approval(
         address indexed _owner,
-        address indexed _spender,
+        address indexed _spender, 
         uint _value
     );
 
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance; // 주소를 확인 후 매핑.
-
+    mapping(address => mapping(address => uint256)) public allowance;
+    
     constructor() public {
         balanceOf[msg.sender] = totalSupply;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
+        // require that the value is greater or equal for transfer
         require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value; // 메시지 발신자 잔액에서 전송하는 토큰 값 뺌.
-        balanceOf[_to] += _value; // 전송하는 값 더함.
-        emit Transfer(msg.sender, _to, _value); 
+         // transfer the amount and subtract the balance
+        balanceOf[msg.sender] -= _value;
+        // add the balance
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    function approve(address _spender, uint _value) public returns (bool success) {
-        allowance[msg.sender][_spender]= _value;
-        emit Approval(msg.sender, _spender, _value); // 발신자 주소와 같아야함.
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        
-        // 값이 발신자 잔액 이하가 되도록 조건 설정.
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
-        
-        balanceOf[_to] += _value; // transferFrom 실행, 현재 잔액에 전송받을 값을 더한 수량.
-        balanceOf[_from] -= _value; // transferFrom 실행, 현재 잔액에 전송받을 값을 뺀 수량.
-        
-        // 발신자가 허용한 호출자의 allowance를 수량에서 제외한다.
-        allowance[msg.sender][_from] -= _value; // 주소로 메시지 발신자 확인, 값에서 뺀 수량.
+        // add the balance for transferFrom
+        balanceOf[_to] += _value;
+        // subtract the balance for transferFrom
+        balanceOf[_from] -= _value;
+        allowance[msg.sender][_from] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
