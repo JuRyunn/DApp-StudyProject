663b027183df0ddbf096d50bf154a1b7b7617e5a
JuRyunn <wngus2991@gmail.com>
2022-08-20 05:59:54 +0900

    14th Commit
    React basic interface: 128

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react'
+import './App.css'
+import Navbar from './Navbar';
 
 class App extends Component {
 
-    // React Code
-    render() {
-        return {
-            <div>
-            
-            </div>
+    constructor(props) {
+        super(props)
+        this.state= {
+            account: '0x0'
         }
     }
+
+    // React Code
+    render() {
+        return (
+        <div>      
+            <Navbar account= {this.state.account} />
+                <div className='text-center'>
+                    <h></h>
+                </div>
+        </div>
+        )
+    }
 }
+
+export default App;
